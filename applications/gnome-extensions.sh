c7e8e35dc348d85033665d82596e514071080b7e
Samuel Rezende <samuelferreirarezende@gmail.com>
2025-12-09 14:12:01 -0300

    feat: gnome extensions

---
+++ applications/gnome-extensions.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+sudo apt install -y gnome-shell-extension-manager pipx
+pipx ensurepath
+pipx install gnome-extensions-cli --system-site-packages
+
+gnome-extensions disable tiling-assistant@ubuntu.com
+gnome-extensions disable ubuntu-appindicators@ubuntu.com
+gnome-extensions disable ubuntu-dock@ubuntu.com
+gnome-extensions disable ding@rastersoft.com
+
+gext install tactile@lundal.io
+gext install just-perfection-desktop@just-perfection
+gext install blur-my-shell@aunetx
+gext install space-bar@luchrioh
+gext install appindicatorsupport@rgcjonas.gmail.com
+gext install clipboard-indicator@tudmotu.com
+gext install unblank@sun.wxg@gmail.com
+gext install windowIsReady_Remover@nunofarruca@gmail.com
+
+sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/appindicatorsupport\@rgcjonas.gmail.com/schemas/org.gnome.shell.extensions.appindicator.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/clipboard-indicator\@tudmotu.com/schemas/org.gnome.shell.extensions.clipboard-indicator.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/unblank\@sun.wxg\@gmail.com/schemas/org.gnome.shell.extensions.unblank.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Configure Tactile
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-1 2
+gsettings set org.gnome.shell.extensions.tactile col-2 1
+gsettings set org.gnome.shell.extensions.tactile col-3 0
+gsettings set org.gnome.shell.extensions.tactile row-0 1
+gsettings set org.gnome.shell.extensions.tactile row-1 1
+gsettings set org.gnome.shell.extensions.tactile gap-size 32
+
+# Configure Just Perfection
+gsettings set org.gnome.shell.extensions.just-perfection animation 2
+gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+gsettings set org.gnome.shell.extensions.just-perfection workspace true
+gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+
+# Configure Space Bar
+gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
