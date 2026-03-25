1a1c3d0a606a1e325451149a90a9f00fa155d5c6
Samuel Rezende <samuelferreirarezende@gmail.com>
2026-03-24 22:57:50 -0300

    feat: flameshot

--- applications/vicinae.sh
+++ applications/vicinae.sh
@@ -1,4 +1,4 @@
-sudo dnf copr enable quadratech188/vicinae
-sudo dnf install vicinae
+sudo dnf copr enable quadratech188/vicinae -y
+sudo dnf install vicinae -y
 
 systemctl --user enable vicinae --now
