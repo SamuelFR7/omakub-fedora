7412e7be8873fae1f6cc8db83c70ed83797b43d6
Samuel Rezende <samuelferreirarezende@gmail.com>
2026-09-15 17:11:47 -0300

    feat: wireplumber

---
+++ applications/wireplumber.sh
@@ -0,0 +1,4 @@
+mkdir -p "$HOME/.config/wireplumber"
+cp files/99-headset-softvol.conf "$HOME/.config/wireplumber/"
+mkdir -p "$HOME/.config/wireplumber/wireplumber.conf.d"
+cp files/99-headset-softvol.conf "$HOME/.config/wireplumber/wireplumber.conf.d/"
