3ee7e67f75204be9452d465658b8f1ae81cdbfba
Samuel Rezende <samuelferreirarezende@gmail.com>
2026-09-16 22:50:47 -0300

    fix(terminal): op-cli use beta

--- terminal/one-password.sh
+++ terminal/one-password.sh
@@ -1,3 +1,3 @@
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-sudo dnf check-update -y 1password-cli && sudo dnf install 1password-cli -y
+sudo sh -c 'echo -e "[1password-beta]\nname=1Password Beta Channel\nbaseurl=https://downloads.1password.com/linux/rpm/beta/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\nincludepkgs=1password-cli\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password-beta.repo'
+sudo dnf check-update -y 1password-cli; sudo dnf install 1password-cli -y
